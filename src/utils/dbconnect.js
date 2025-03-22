49842d2179cf97964fdb9ba2040c5dbcf52a4b3a
PadalaDevisrisairam <devisri.padala@sasi.ac.in>
2025-03-22 15:46:26 +0530

    version 2

--- src/utils/dbconnect.js
+++ src/utils/dbconnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb+srv://Devisri123:devi+123@cluster0.vduzx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = "mongodb://localhost:27017/StockMaintainanceSystem";
 
 const dbconnect = async () => {
   
